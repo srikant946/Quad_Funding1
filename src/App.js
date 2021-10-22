7c5f13a0aa0c6a5feb175e70239543fb9e8030c0
Srikant <kamathsrikant@gmail.com>
2021-10-22 13:39:18 +0530

    Commit #2

--- src/App.js
+++ src/App.js
@@ -2,32 +2,59 @@ import './App.css';
 import Card from './Cards/Cards';
 import Footer from './Footer/foot';
 import Form from './Form/Grant_Form';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faSearch, faShoppingCart, faWallet } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <>
-    {/* <div className = "page-container">
+    <div className = "page-container">
           <div className = "content-wrap">
 
     <div className="container-fluid px-0">
-    <nav className="navbar navbar-expand-sm navbar-dark bg-black py-0 px-0"> <a className="navbar-brand" href="#"><img id="logo" src="https://i.imgur.com/K7Nwq4V.jpg"/> &nbsp;&nbsp;&nbsp;Acme Inc</a> <span className="v-line"></span> <button className="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-black py-0 px-0"> <a className="navbar-brand" href="#"><img id="logo" src="https://i.imgur.com/K7Nwq4V.jpg"/> &nbsp;&nbsp;&nbsp;EthQF</a> <span className="v-line"></span> <button className="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-                <li className="nav-item active"> <a className="nav-link" href="#">Programs</a> </li>
-                <li className="nav-item"> <a className="nav-link" href="#">Users</a> </li>
-                <li className="nav-item"> <a className="nav-link" href="#">Coaching</a> </li>
-                <li className="nav-item"> <a className="nav-link" href="#">Analytics</a> </li>
+                <li className="nav-item active"> <a className="nav-link ms-auto" href="#">What's Possible</a> </li>
+                <li className="nav-item"> <a className="nav-link" href="#">Products</a> </li>
+                <li className="nav-item"> <a className="nav-link" href="#">Community</a> </li>
+                <li className="nav-item"> <a className="nav-link" href="#">Organization</a> </li>
+            </ul>
+            <ul class="nav navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></a> 
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></a> 
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><FontAwesomeIcon icon={faBell}></FontAwesomeIcon></a> 
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><FontAwesomeIcon icon={faWallet}></FontAwesomeIcon></a> 
+                </li>
+                <ul class="nav header-navbar-rht">
+                    <li class="nav-item dropdown has-arrow logged-item">
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                        <span class="user-img">
+                            <img class="rounded-circle" src="https://i.imgur.com/K7Nwq4V.jpg" width="31" alt="Img"/>&nbsp; ABCD
+                        </span>
+                    </a>                
+                    </li>
+                </ul>
             </ul>
         </div>
     </nav>
-</div>
+    </div>
 
     <Card/>
-</div>
-</div>
-    <Footer/> */}
+    
+    </div>
+    </div>
+    
+    <Footer/>
 
-    <Form/>
+    {/* <Form/> */}
     </>
   );
 }
--- src/Footer/foot.jsx
+++ src/Footer/foot.jsx
@@ -1,43 +1,104 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import "./footer-style.css";
 
 const Footer = () => {
   return (
-    <div className="mainFooter">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h4>Hello</h4>
-            <ul className="list-unstyled">
-              <li>3000 --00 </li>
-              <li>Ok</li>
-              <li>Hi</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>Hello</h4>
-            <ul className="list-unstyled">
-              <li>3000 --00 </li>
-              <li>Ok</li>
-              <li>Hi</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>Hello</h4>
-            <ul className="list-unstyled">
-              <li>3000 --00 </li>
-              <li>Ok</li>
-              <li>Hi</li>
-            </ul>
+    <div class="d-flex flex-column h-100">
+      <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+          <div class="row gy-4 gx-5">
+            <div class="col-lg-4 col-md-6">
+              <h5 class="h1 text-white">EthQF</h5>
+              <p class="small text-muted">
+                EthQF is a platform where you get paid to work on open source
+                software in Python, Rust, Ruby, JavaScript, Solidity, HTML, CSS,
+                Design, and more.
+              </p>
+              <p class="small text-muted mb-0">
+                &copy; Copyrights. 2021. All rights reserved.{" "}
+              </p>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3">Product</h5>
+              <ul class="list-unstyled text-muted">
+                <li>
+                  <a href="#">Issue Explorer</a>
+                </li>
+                <li>
+                  <a href="#">Kudos</a>
+                </li>
+                <li>
+                  <a href="#">Grants</a>
+                </li>
+                <li>
+                  <a href="#">Quests</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3">Organization</h5>
+              <ul class="list-unstyled text-muted">
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Mission</a>
+                </li>
+                <li>
+                  <a href="#">Results</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3">Community</h5>
+              <ul class="list-unstyled text-muted">
+                <li>
+                  <a href="#">Code Of Conduct</a>
+                </li>
+                <li>
+                  <a href="#">Support</a>
+                </li>
+                <li>
+                  <a href="#">Store</a>
+                </li>
+                <li>
+                  <a href="#">Discord</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3 text-center">Catch Us On</h5>
+              <div className="social-container my-3">
+                <a href="https://www.youtube.com/" className="youtube social">
+                  <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+                <a href="https://www.facebook.com/" className="facebook social">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.twitter.com/" className="twitter social">
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a
+                  href="https://www.instagram.com/"
+                  className="instagram social"
+                >
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <hr />
-        <div className="row text-center">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} ABC | All Rights Reserved | Ok.
-          </p>
-        </div>
-      </div>
+      </footer>
     </div>
   );
 };
